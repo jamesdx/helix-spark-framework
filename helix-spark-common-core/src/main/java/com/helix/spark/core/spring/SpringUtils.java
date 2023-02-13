bc8e0a251cd7f4a3fb4d51323ae02979aacca28b
Allen <allen_wang@hgplan.cn>
2023-02-13 15:13:42 +0800

    hps-001: add helix spark core project.

---
+++ helix-spark-common-core/src/main/java/com/helix/spark/core/spring/SpringUtils.java
@@ -0,0 +1,60 @@
+package com.helix.spark.core.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * All rights Reserved, Designed By www.helix.cn
+ *
+ * @author [Allen Wang]
+ * @version V1.0
+ * Copyright 2023 www.helix.cn Inc. All rights reserved.
+ * @date 2023/2/13 14:54
+ **/
+public class SpringUtils implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringUtils.context == null) {
+            context = applicationContext;
+        }
+    }
+
+    /**
+     * 通过字节码获取
+     *
+     * @param beanClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> beanClass) {
+        return context.getBean(beanClass);
+    }
+
+    /**
+     * 通过BeanName获取
+     *
+     * @param beanName
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String beanName) {
+        return (T) context.getBean(beanName);
+    }
+
+    /**
+     * 通过beanName和字节码获取
+     *
+     * @param name
+     * @param beanClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> beanClass) {
+        return context.getBean(name, beanClass);
+    }
+
+}
